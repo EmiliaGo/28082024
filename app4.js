e66b7e364c2885bbb6b7549e97788387459a0643
EmiliaGo <emiliagonzalez71@aragon.unam.mx>
2024-09-04 23:31:48 -0600

    repositorio suma

---
+++ app4.js
@@ -0,0 +1,18 @@
+/*
+funtion suma(num1,num2){
+    return parseInt(num1)+parseInt(num2);
+}
+
+*/
+
+let num1 = parseInt(prompt("Ingresa un numero 1"));
+let num2 = parseInt(prompt("Ingresa un numero 2"));
+//let resultado = suma(num1,num2)
+
+function suma(num1,num2){
+    return num1+num2;
+}
+
+//
+
+console.log("La suma de "+num1+" y "+num2+" es: "+suma(num1,num2));
